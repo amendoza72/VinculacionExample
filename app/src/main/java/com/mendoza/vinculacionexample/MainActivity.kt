787f7718e8da1740e147d291ab1b0167b9df9ea1
Antonio Mendoza <antonio.mendoza@gmail.com>
2024-02-06 13:04:44 +0100

    Ejemplo con TextView

--- app/src/main/java/com/mendoza/vinculacionexample/MainActivity.kt
+++ app/src/main/java/com/mendoza/vinculacionexample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.mendoza.vinculacionexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mendoza.vinculacionexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var enlace: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enlace = ActivityMainBinding.inflate(layoutInflater)
+        val vistaPrincipal = enlace.root
+        setContentView(vistaPrincipal)
+
+        enlace.helloWorld.text = "Hola!!!"
     }
 }
